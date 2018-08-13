d05ef2870f0a8202eaa6034a0e13a8b107bf3187
corestrike <corestrike@gmail.com>
2018-08-13 13:33:39 +0900

    リパッケージ

--- src/main/java/todo/interfaces/HelloWorldController.java
+++ src/main/java/todo/interfaces/HelloWorldController.java
@@ -1,4 +1,4 @@
-package todo.sample.interfaces;
+package todo.interfaces;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
