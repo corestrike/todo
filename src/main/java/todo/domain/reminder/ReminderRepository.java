baa46a33caa8634e457715874afd3ead6e1ddb02
mikami <daisuke_mikami@unirita.co.jp>
2018-11-20 23:01:25 +0900

    ReminderテーブルのタスクIDを一意化

--- src/main/java/todo/domain/reminder/ReminderRepository.java
+++ src/main/java/todo/domain/reminder/ReminderRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReminderRepository extends org.springframework.data.repository.Repository<Reminder, Integer> {
 	Reminder findById(Integer id);
+	Reminder findByTaskId(Integer taskId);
 	<S extends Reminder> S save(S var1);
 	void deleteById(Integer id);
 }
